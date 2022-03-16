f68de2f31d1b369910d218d2337a40efd557e79d
Surya Teja Palavalsa <mail@suryateja.dev>
2022-03-16 16:19:15 -0700

    SSO login

--- zotattendance.py
+++ zotattendance.py
@@ -12,20 +12,32 @@ def assets(static_file):
     '''Send other static files like images under the assets/ directory.'''
     return send_from_directory("static/assets", static_file)
 
-@app.route("/api/login", methods=['POST'])
-def api():
-    return jsonify({
-        "success": False,
-        "message": "testing"
-    })
 
 @app.route("/api/login", methods=["GET"])
 def login():
-    cookies = ""
-    for k, v in enumerate(request.cookies):
-        cookies = k+":"+v
-    return cookies
+    '''return path after SSO Login validates UCI cookies and gets user details'''
+    webauth_cookie = request.cookies.get("ucinetid_auth")
+    try:
+        user_details = get_user_details(webauth_cookie)
+    except Exception as e:
+        return "login Failed"
 
+    return user_details
+
+
+def get_user_details(webauth_cookie: str)->dict:
+    '''Gets User Details from webauth_cookie'''
+    url = "https://login.uci.edu/ucinetid/webauth_check?ucinetid_auth="+webauth_cookie+"&return_xml=true"
+    r = requests.get(url)
+    if r.status_code != 200:
+        raise(Exception("Login Validation Failed"))
+
+    root = ET.fromstring(r.text)
+    user_details = {}
+    for child in root:
+        user_details[child.tag] = child.text
+
+    return user_details
 
 if __name__ == "__main__":
     app.run()
