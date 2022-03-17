76aa2e8c13e1502ec6df79d87b351c8048306a18
Yizhen Liu <yliu2019@uci.edu>
2022-03-16 19:56:11 -0700

    Emergency fix for login

--- plugin.py
+++ plugin.py
@@ -1,7 +1,7 @@
 from plugins import popquiz
 
-def handle_plugin_request(plugin_name,data,session):
+def handle_plugin_request(plugin_name, data, session):
     if plugin_name == 'popquiz':
-        popquiz.handle(data,session)
+        popquiz.handle(data, session)
     else:
         raise Exception("Unsupported Plugin!")
--- zotattendance.py
+++ zotattendance.py
@@ -13,7 +13,7 @@ app.secret_key = b'32f87d601a8fb7d44a86f3549c2e62858a4ac74f48b59edeec9449fed58be
 @app.route("/")
 def show_index_or_home():
     '''Send index.html or home.html depending on whether users logged in or not.'''
-    if 'logged_in' not in session:
+    if 'user_record' not in session:
         return send_from_directory("static", "index.html")
     else:
         return send_from_directory("static", "home.html")
@@ -28,9 +28,8 @@ def login():
     '''Return path after SSO Login validates UCI cookies and gets user details.'''
     try:
         user_details = get_user_details(request.cookies.get("ucinetid_auth"))
-        session['logged_in'] = True
         session['user_record'] = get_user_record(user_details)
-        return redirect(url_for("/"), code=200)
+        return send_from_directory("static", "home.html")
     except Exception as e:
         app.logger.error(f'{asctime()} {repr(e)}')
         return send_from_directory("static", "error.html")
@@ -38,11 +37,14 @@ def login():
 @app.route("/api/plugins/<plugin_name>")
 def plugins(plugin_name: str):
     try:
-        handle_plugin_request(plugin_name,request.args,session)
-        return "Success", 200
+        handle_plugin_request(plugin_name, request.args, session)
+        return jsonify({ "success": True })
     except Exception as e:
         app.logger.error(f'{asctime()} {repr(e)}')
-        return "Bad Request",400
+        return jsonify({
+            "success": False,
+            "message": "Failed to Check in."
+        }), 400
 
 @app.route("/api/courses")
 def courses():
@@ -50,7 +52,10 @@ def courses():
         return jsonify(session['user_record']['courses'])
     except Exception as e:
         app.logger.error(f'{asctime()} {repr(e)}')
-        return "Bad Request",400
+        return jsonify({
+            "success": False,
+            "message": "Failed to retrieve course list."
+        }), 400
 
 def get_user_details(webauth_cookie: str) -> dict:
     '''Gets User Details from webauth_cookie'''
